c424e38829171ebb693214ba57cd64679ea235c1
Phong Lu <lutaiphong@gmail.com>
2023-10-15 19:10:12 -0400

    Complete User page.

--- lms-portal/src/components/User/User.js
+++ lms-portal/src/components/User/User.js
@@ -1,66 +1,153 @@
-import { Button, Card, FormControl, InputLabel, MenuItem, Pagination, Select, TextField } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2";
-import { useEffect, useState } from "react";
-import DataPagination from "../DataPagination/DataPagination";
-import { useDispatch } from "react-redux";
-import { getAllUsers } from "services/AdminService";
+import {
+    Button,
+    Card,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    FormLabel,
+    InputLabel,
+    MenuItem,
+    Select,
+    TextField
+} from '@mui/material';
+import { debounce } from 'lodash';
+import Grid from '@mui/material/Unstable_Grid2';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { editUser, getAllUsers, getUser } from 'services/AdminService';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import ActionIcon from 'components/ActionIcon/ActionIcon';
+import EditIcon from '@mui/icons-material/Edit';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { SnackbarCustom } from 'components/SnackbarCustom/SnackbarCustom';
 
-export default function Profile(props) {
-    let {users, itemPerPage} = props;
-    users = users || [];
-
-    let [page, setPage] = useState(1);
+const Profile = () => {
+    const [list, setList] = useState([]);
+    const [searchText, setSearchText] = useState('');
+    const [id, setId] = useState('');
     const [username, setUsername] = useState('');
     const [role, setRole] = useState('User');
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-    const count = Math.ceil(users.length / itemPerPage);
-    const DATA_PAGINATION = DataPagination(users, itemPerPage);
-
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [address, setAddress] = useState('');
+    const [numOfOverdues, setNumOfOverdues] = useState(0);
+    const [status, setStatus] = useState('Active');
     const dispatch = useDispatch();
+    const [alertContent, setAlertContent] = useState('');
+    const [openAlert, setOpenAlert] = useState(false);
+
+    useEffect(() => search(), []);
 
     useEffect(() => {
-        dispatch(getAllUsers());
-    }, []);
+        search(searchText);
+    }, [searchText]);
 
-    useEffect(() => {
-        gotoPage(1);
-    }, [users]);
+    const getSearchResults = useCallback(
+        debounce(value =>
+            dispatch(getAllUsers(value))
+                .then(res => {
+                    if (res.payload && res.payload.status_code == 200) {
+                        setList(res.payload.data);
+                    }
+                }), 800), []
+    );
 
-    const onUsernameChange = (e) => {
-        setUsername(e.target.value);
+    const search = searchText => {
+        getSearchResults(searchText);
     }
 
-    const onRoleChange = (e) => {
-        setRole(e.target.value);
-    }
+    const onSearchTextChange = e => setSearchText(e.target.value);
 
-    const onFirstNameChange = (e) => {
-        setFirstName(e.target.value);
-    }
+    const onUsernameChange = e => setUsername(e.target.value);
 
-    const onLastNameChange = (e) => {
-        setLastName(e.target.value);
-    }
+    const onRoleChange = e => setRole(e.target.value);
 
-    const gotoPage = (page) => {
-        setPage(page);
-        DATA_PAGINATION.goTo(page);
-    }
+    const onFirstNameChange = e => setFirstName(e.target.value);
 
-    const pageChanged = (e, page) => {
-        gotoPage(page);
-    }
+    const onLastNameChange = e => setLastName(e.target.value);
 
-    const handleDelete = (id) => {
-        if (typeof props.setRefresh === "function") {
-            props.setRefresh(true);
-        }
-    }
+    const onEmailChange = e => setEmail(e.target.value);
 
-    return (
-        <div className="container">
-            <h3>Profile Management</h3>
+    const onPhoneNumberChange = e => setPhoneNumber(e.target.value);
+
+    const onAddressChange = e => setAddress(e.target.value);
+
+    const onStatusChange = e => setRole(e.target.value);
+
+    const onSubmit = e => {
+        const user = {
+            id,
+            userName: username,
+            roleCd: role,
+            firstName,
+            lastName,
+            email,
+            phoneNumber,
+            address,
+            status
+        };
+
+        dispatch(editUser({id, user}))
+            .then(res => {
+                if (res.payload && res.payload.status_code == 200) {
+                    setAlertContent(res.payload.message);
+                    setOpenAlert(true);
+                    resetForm();
+                    search(searchText);
+                }
+            });
+    };
+
+    const onEditClick = id =>
+        dispatch(getUser(id))
+            .then(res => {
+                if (res.payload && res.payload.status_code == 200) {
+                    const user = res.payload.data;
+
+                    if (user) {
+                        setId(user.id);
+                        setUsername(user.userName);
+                        setRole(user.roleCd);
+                        setFirstName(user.firstName);
+                        setLastName(user.lastName);
+                        setEmail(user.email);
+                        setPhoneNumber(user.phoneNumber);
+                        setAddress(user.address);
+                        setStatus(user.status);
+                        setNumOfOverdues(user.numOfOverdues);
+                    }
+                }
+            });
+
+    const onDeleteClick = id => console.log(id);
+
+    const resetForm = () => {
+        setId(null);
+        setUsername('');
+        setRole('User');
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPhoneNumber('');
+        setAddress('');
+        setStatus('Active');
+        setNumOfOverdues(0);
+    };
+
+    const isEdit = id != null && id != undefined;
+
+    return <div className="container">
+        <h3>Profile Management</h3>
+        <div className="row">
             <div className="col-md-4">
                 <Card variant="outlined">
                     <form className="form">
@@ -68,6 +155,7 @@ export default function Profile(props) {
                             <div className="col-md-6">
                                 <FormControl fullWidth className="form-control-field">
                                     <TextField
+                                        disabled={isEdit}
                                         label="Username" type="search"
                                         onChange={onUsernameChange}
                                         value={username}
@@ -82,7 +170,7 @@ export default function Profile(props) {
                                         value={role}
                                         label="Role"
                                         onChange={onRoleChange}>
-                                        <MenuItem value="Profile">Student</MenuItem>
+                                        <MenuItem value="User">Student</MenuItem>
                                         <MenuItem value="Admin">Admin</MenuItem>
                                         <MenuItem value="Librarian">Librarian</MenuItem>
                                     </Select>
@@ -110,33 +198,110 @@ export default function Profile(props) {
                             </div>
                         </div>
                         <div className="row">
+                            <div className="col-md-6">
+                                <FormControl fullWidth className="form-control-field">
+                                    <TextField
+                                        label="Email" type="search"
+                                        onChange={onEmailChange}
+                                        value={email}
+                                        fullWidth />
+                                </FormControl>
+                            </div>
+                            <div className="col-md-6">
+                                <FormControl fullWidth className="form-control-field">
+                                    <TextField
+                                        label="Phone number" type="search"
+                                        onChange={onPhoneNumberChange}
+                                        value={phoneNumber}
+                                        fullWidth />
+                                </FormControl>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <FormControl fullWidth className="form-control-field">
+                                    <TextField
+                                        label="Address" type="search"
+                                        onChange={onAddressChange}
+                                        value={address}
+                                        fullWidth />
+                                </FormControl>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <FormControl fullWidth className="form-control-field">
+                                    <InputLabel id="statusLabel">Status</InputLabel>
+                                    <Select
+                                        labelId="statusLabel"
+                                        value={status}
+                                        label="Status"
+                                        onChange={onStatusChange}>
+                                        <MenuItem value="Active">Active</MenuItem>
+                                        <MenuItem value="Locked">Locked</MenuItem>
+                                        <MenuItem value="Suspended">Suspended</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
+                        </div>
+                        <div className="row">
                             <div className="col-md-12">
                                 <Grid container justifyContent="center">
-                                    <Button variant="contained" color="primary">Save</Button>
+                                    <Button variant="contained" color="primary" onClick={onSubmit}>Save</Button>
                                 </Grid>
                             </div>
                         </div>
                     </form>
                 </Card>
             </div>
-            <Grid container rowSpacing={3} columnSpacing={{xs: 1, sm: 2, md: 3}}>
-                {DATA_PAGINATION.currentData().map(prop =>
-                    <Grid key={prop.id} xs={12} md={6} lg={4} xl={3}>
-                        {/*<PropertyCard {...prop} roles={roles} {...others} deletedFunc={() => handleDelete(prop.id)} />*/}
-                    </Grid>
-                )}
-            </Grid>
-            {count > 0 && (
-                <div className="pagination">
-                    <Pagination
-                        count={count}
-                        size="large"
-                        color="primary"
-                        page={page}
-                        onChange={pageChanged}
-                    />
+            <div className="col-md-8">
+                <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', paddingBottom: '10px'}}>
+                    <FormLabel style={{paddingRight: '10px', minWidth: '80px'}}>Search</FormLabel>
+                    <TextField fullWidth value={searchText} onChange={onSearchTextChange} />
                 </div>
-            )}
+                <TableContainer component={Paper}>
+                    <Table sx={{minWidth: 650}} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Username</TableCell>
+                                <TableCell>First name</TableCell>
+                                <TableCell>Last name</TableCell>
+                                <TableCell>Role</TableCell>
+                                <TableCell>Overdue amount</TableCell>
+                                <TableCell>&nbsp;</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {list.map(row => <TableRow
+                                key={row.id}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.userName}
+                                </TableCell>
+                                <TableCell>{row.firstName}</TableCell>
+                                <TableCell>{row.lastName}</TableCell>
+                                <TableCell>{row.roleCd}</TableCell>
+                                <TableCell>{row.numOfOverdues || 0}</TableCell>
+                                <TableCell>
+                                    <ActionIcon icon={<EditIcon onClick={() => onEditClick(row.id)} />} />
+                                    <ActionIcon icon={<CancelIcon onClick={() => onDeleteClick(row.id)} />} />
+                                </TableCell>
+                            </TableRow>)}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
         </div>
-    )
-}
+        <SnackbarCustom
+            vertical="top"
+            horizontal="right"
+            open={openAlert}
+            autoHideDuration={2000}
+            severity="success"
+            closed={() => setOpenAlert(!openAlert)}
+        >{alertContent}</SnackbarCustom>
+    </div>;
+};
+
+export default Profile;
--- lms-portal/src/services/AdminService.js
+++ lms-portal/src/services/AdminService.js
@@ -4,20 +4,34 @@ import axiosInstance from "./AxiosService";
 export const getAllBooks = createAsyncThunk(
     'books/getAll',
     async () => {
-        const result = await axiosInstance.adminService.get('/lms/admin/books/search?q=', { withCredentials: true });
+        const result = await axiosInstance.adminService.get('/lms/admin/books/search?q=', {withCredentials: true});
         return result.data;
     });
 
 export const getAllCategories = createAsyncThunk(
     'categories/getAll',
     async () => {
-        const result = await axiosInstance.adminService.get('/lms/admin/book_categories/search?q=', { withCredentials: true });
+        const result = await axiosInstance.adminService.get('/lms/admin/book_categories/search?q=', {withCredentials: true});
+        return result.data;
+    });
+
+export const editUser = createAsyncThunk(
+    'users/edit',
+    async ({id, user}) => {
+        const result = await axiosInstance.adminService.put('/lms/admin/users/' + id, user, {withCredentials: true});
         return result.data;
     });
 
 export const getAllUsers = createAsyncThunk(
     'users/getAll',
-    async () => {
-        const result = await axiosInstance.adminService.get('/lms/admin/users/search?q=', { withCredentials: true });
+    async (searchText) => {
+        const result = await axiosInstance.adminService.get('/lms/admin/users/search?q=' + searchText, {withCredentials: true});
+        return result.data;
+    });
+
+export const getUser = createAsyncThunk(
+    'users/getById',
+    async (id) => {
+        const result = await axiosInstance.adminService.get('/lms/admin/users/' + id, {withCredentials: true});
         return result.data;
     });
