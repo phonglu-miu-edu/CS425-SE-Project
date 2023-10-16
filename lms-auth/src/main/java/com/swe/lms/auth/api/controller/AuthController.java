b33a92ba53b3bd7750e54c3691aeb46813d2fb54
baonguyen5759 <quocnb@gmail.com>
2023-10-15 21:42:14 -0500

    optimized imports

--- lms-auth/src/main/java/com/swe/lms/auth/api/controller/AuthController.java
+++ lms-auth/src/main/java/com/swe/lms/auth/api/controller/AuthController.java
@@ -6,7 +6,10 @@ import com.swe.lms.auth.api.service.IAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletRequest;
 
