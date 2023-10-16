f46c4a75fcefd0e87f42537247ca33cd6188195a
baonguyen5759 <quocnb@gmail.com>
2023-10-15 21:49:36 -0500

    initialize Book Service

---
+++ lms-book/src/main/java/com/swe/lms/book/api/feign/IAuthFeignClient.java
@@ -0,0 +1,14 @@
+package com.swe.lms.book.api.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+
+@FeignClient("AuthService")
+public interface IAuthFeignClient {
+    @PostMapping(value = "/lms/auth/verify")
+    ResponseEntity<Map<String, Object>> verify(@RequestBody String token);
+}
