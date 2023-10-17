bfe0c36e9242f9c0bb330ea3f8d75849845d35a9
deivison m <deivisonmozer@gmail.com>
2023-10-17 14:48:03 -0400

    commit

--- lms-book/src/test/java/com/swe/lms/book/api/BookServiceUnitTest.java
+++ lms-book/src/test/java/com/swe/lms/book/api/BookServiceUnitTest.java
@@ -3,7 +3,7 @@ package com.swe.lms.book.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.swe.lms.book.api.controller.BookController;
-import com.swe.lms.book.api.model.Book;
+import com.swe.lms.book.api.dto.CheckoutRecordDTO;
 import com.swe.lms.book.api.service.IBookService;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,14 +12,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,6 +35,8 @@ public class BookServiceUnitTest {
     private IBookService bookService;
     @InjectMocks
     private BookController bookController;
+
+    CheckoutRecordDTO checkoutRecordDTO = new CheckoutRecordDTO(1,1,1,1,"Checkout record", LocalDate.now(),LocalDate.of(2023,12,10));
     @Before
     public void setUp(){
         MockitoAnnotations.initMocks(this);
@@ -47,14 +50,14 @@ public class BookServiceUnitTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is4xxClientError());
     }
-    @Test
-    public void TestBookCopiesPostWithBody() throws Exception {
-        List<Integer> bookId = new ArrayList<Integer>(Arrays.asList(1,2,3));
-        mockMvc.perform(MockMvcRequestBuilders
-                .post("/lms/book/checkout/")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(this.objectMapper.writeValueAsString(bookId))).andExpect(status().isOk());
-    }
+//    @Test
+//    public void TestBookCopiesPostWithBody() throws Exception {
+//        List<Integer> bookId = new ArrayList<Integer>(Arrays.asList(1,2,3));
+//        mockMvc.perform(MockMvcRequestBuilders
+//                .post("/lms/book/checkout/")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(this.objectMapper.writeValueAsString(checkoutRecordDTO))).andExpect(status().isOk());
+//    }
     @Test
     public void TestBookCopiesPutWithBody() throws Exception {
 
